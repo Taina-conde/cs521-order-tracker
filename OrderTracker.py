9d0a99fa89baf686e6c28395ac28f8170256f798
Taina Conde <tainabatistaconde@gmail.com>
2021-10-19 15:16:08 -0400

    feat: OrderTracker - add name attribute and __get_menu method

--- OrderTracker.py
+++ OrderTracker.py
@@ -1,6 +1,9 @@
 class OrderTracker():
-    def __init__(self, menu_dict, orders_list):
-        self.menu = menu_dict
+    def __init__(self,restaurant_name = '', menu_dict = {}, orders_list = []):
+        self.name = restaurant_name
+        self.__menu = menu_dict
         self.orders = orders_list
-        
+    def __get_menu(self):
+        return self.__menu
+
 
