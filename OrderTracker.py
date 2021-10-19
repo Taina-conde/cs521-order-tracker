9b2fa66369608854e83b85ff72077e86fbd24d6d
Taina Conde <tainabatistaconde@gmail.com>
2021-10-19 15:21:31 -0400

    feat: create __repr__ method

--- OrderTracker.py
+++ OrderTracker.py
@@ -3,7 +3,13 @@ class OrderTracker():
         self.name = restaurant_name
         self.__menu = menu_dict
         self.orders = orders_list
+    def __repr__(self):
+        return (
+            f'{self.name} restaurant has'
+            f'{len(self.orders)} orders beeing prepared'
+        )
     def __get_menu(self):
         return self.__menu
+    
 
 
