d002c6d2516e217890abd77969fa29710c20d858
Taina Conde <tainabatistaconde@gmail.com>
2021-10-19 15:05:06 -0400

    feat: add menu and orders attribute

--- OrderTracker.py
+++ OrderTracker.py
@@ -1,3 +1,6 @@
 class OrderTracker():
-    def __init__(self):
-        pass
+    def __init__(self, menu_dict, orders_list):
+        self.menu = menu_dict
+        self.orders = orders_list
+        
+
