2ca680fdcaa49f753878d9253a5993dc09512da3
Taina Conde <tainabatistaconde@gmail.com>
2021-10-19 15:29:41 -0400

    feat: create unit tests for Ordertracker

--- OrderTracker.py
+++ OrderTracker.py
@@ -5,11 +5,18 @@ class OrderTracker():
         self.orders = orders_list
     def __repr__(self):
         return (
-            f'{self.name} restaurant has'
+            f'{self.name} restaurant has '
             f'{len(self.orders)} orders beeing prepared'
         )
     def __get_menu(self):
         return self.__menu
     
-
-
+# Unit tests
+if __name__ == '__main__':
+    carmines_restaurant = OrderTracker('Carmines', {
+        'Margerita Pizza': 25,
+        'Seafood linguine': 40.5,
+        'Bruschetta': 16.55,
+        'Tiramissu cake': 9,
+    })
+    print(carmines_restaurant)
